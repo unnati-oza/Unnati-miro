50caa8d007bcae9b04449b4aa1a21256cc1db22c
unnati-oza <unnatioza2003@gmail.com>
2023-10-24 22:00:03 +0530

    Page

---
+++ +page.svelte
@@ -0,0 +1,29 @@
+<script>
+	import Note from '../Note.svelte';
+	
+	let notes = [
+		{
+			id: 1,
+			text: 'Enter Text here'
+		},
+		{
+			id: 2,
+			text: 'Enter Text here'
+		}
+	];
+</script>
+
+{#each notes as note}
+	<Note>
+		<div contenteditable>{note.text}</div>
+	</Note>
+{/each}
+
+<style>
+	div {
+		background: lightcoral;
+		padding: 20px;
+		width: 350px;
+		text-align: center;
+	}
+</style>
